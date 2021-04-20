a8ab0c76db403f185c4541f1716326864c17e604
DimonYarkin <dmitryyarkin@yandex.ru>
2021-04-21 00:22:23 +0300

    Начало разработки

---
+++ static/js/scripts.js
@@ -0,0 +1,9 @@
+"use strict";
+function addHeader(srtHead) {
+    const headDoc = document.querySelector('.header_h')
+    headDoc.innerHTML = `<h1> ${srtHead} </h1>`
+
+}
+
+
+addHeader('Начало разработки моей crm')
